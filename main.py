52bea4f66cccbf99e3be8664d9c1fe24f539cc18
marvid777 <59563463+marvid777@users.noreply.github.com>
2024-12-06 21:55:42 +0100

    2. Comm def possible

--- main.py
+++ main.py
@@ -12,4 +12,23 @@ grid = [[5,3,0,0,7,0,0,0,0],
 # print(grid)
 print(np.matrix(grid))
 
+def possible(y,x,n) :
+        global grid
+        for i in range(0,9) :
+                if grid[y][i] == n :
+                        return False
+        for i in range(0,9) :
+                if grid[i][x] == n :
+                        return False
+        x0 = (x//3) * 3
+        y0 = (y//3) * 3
+        for i in range (0,3) :
+                for j in range (0,3) :
+                        if grid[y0+i][x0+j] == n :
+                                return False
+        return True
+
+
+
+
 
